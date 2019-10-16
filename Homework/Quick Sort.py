993c70cdc5b4ea00b23f23d2a492135f64106a49
tiffany1020 <33365095+tiffany1020@users.noreply.github.com>
2019-10-17 01:37:09 +0800

    Update Quick Sort.py

--- Homework/Quick Sort.py
+++ Homework/Quick Sort.py
@@ -1,9 +1,10 @@
 def  quicksort(list,begin,last):  
     if begin < last:                              # 如果開始的位置大於結束，則停止
         pivot = partition(list,begin,last)        # 進行第一次劃分 分成左右兩個序列
-        quicksort(list,begin,pivot-1)       # 繼續將左邊序列進行排序 
-        quicksort(list,pivot+1,last)        # 繼續將右邊序列進行排序
+        quicksort(list,begin,pivot-1)             # 繼續將左邊序列進行排序 
+        quicksort(list,pivot+1,last)              # 繼續將右邊序列進行排序
 
+        
 ## 從L開始一個一個值向右檢視、從R開始一個一個值向左檢視
 
 def partition(list,begin,last):
@@ -29,8 +30,8 @@ def partition(list,begin,last):
     list[begin],list[R]=list[R],list[begin]  
     return R
 
-## 測試數值
 
+## 測試數值
 list=[21,4,8,15,30,6,18,1,15,27]
 
 quicksort(list,0,len(list)-1)     # 0 和 len(list)-1 為傳遞list的index值
